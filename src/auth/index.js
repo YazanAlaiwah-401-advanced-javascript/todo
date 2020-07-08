e69c4b022b8f56f30329725221fadeb3d40d6450
YazanAlaiwah <yazanalaiwah@gmail.com>
2020-07-09 00:37:17 +0300

    done

---
+++ src/auth/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { SettingsContext } from '../context/data.js';
+import Show from '../show/index';
+// helper component for ACL
+class Auth extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  static contextType = SettingsContext;
+  render() {
+    let okToRender = false;
+    try {
+      okToRender =
+        this.context.loggedIn &&
+        (this.props.capability
+          ? this.context.user.capabilities.includes(this.props.capability)
+          : true);
+      console.log(
+        this.context.loggedIn,
+        this.props.capability,
+        okToRender,
+        'helo'
+      );
+    } catch (e) {
+      console.log('NOT AUTHORIZED', e.message);
+    }
+    return <Show condition={okToRender}>{this.props.children}</Show>;
+  }
+}
+export default Auth;
--- src/components/todo/todo-connected.js
+++ src/components/todo/todo-connected.js
@@ -4,12 +4,14 @@ import TodoForm from './form.js';
 import TodoList from './list.js';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
-
+import Login from '../../login/index.js';
+import Signup from '../../signup/index.jsx';
+import Auth from '../../auth/index.js';
 import './todo.scss';
 import useAjax from '../../hooks/ajax.js';
 
 // import React, { useContext } from 'react';
-import { SettingsContext } from '../../context/settings.js';
+import { SettingsContext } from '../../context/data.js';
 // import { ThemeContext } from '../../context/theme.js';
 
 function ToDo(props) {
@@ -69,58 +71,64 @@ function ToDo(props) {
 
   return (
     <main>
-      <header>
-        <Navbar bg="primary" variant="dark">
-          <Navbar.Brand>HOME</Navbar.Brand>
+      <Login />
+      <Signup />
+      <Auth capability="read">
+        <header>
+          <Navbar bg="primary" variant="dark">
+            <Navbar.Brand>HOME</Navbar.Brand>
+          </Navbar>
+        </header>
+        <Navbar variant="dark" bg="dark" className="mt-4 ml-5 mr-5">
+          <Navbar.Brand>
+            There are {list.filter((item) => !item.complete).length} Items To
+            Complete
+          </Navbar.Brand>
         </Navbar>
-      </header>
-      <Navbar variant="dark" bg="dark" className="mt-4 ml-5 mr-5">
-        <Navbar.Brand>
-          There are {list.filter((item) => !item.complete).length} Items To
-          Complete
-        </Navbar.Brand>
-      </Navbar>
-      ​
-      <div style={{ textAlign: 'end' }}>
-        <Button
-          style={{}}
-          variant="primary"
-          onClick={(e) => siteContext.setShow(!siteContext.show)}
-        >
-          Hide Completed
-        </Button>
-        <form onChange={(e) => siteContext.changeSort(e.target.value)}>
-          <label class="radio">
-            <input type="radio" name="sort" value="difficulty" />
-            difficulty
-          </label>
-          <label class="radio">
-            <input type="radio" name="sort" value="complete" />
-            complete
-          </label>
-          <label class="radio">
-            <input type="radio" name="sort" value="assignee" />
-            assignee
-          </label>
-          <label class="radio">
-            <input type="radio" name="sort" value="none" />
-            none
-          </label>
-        </form>
-      </div>
-      <section className="todo">
-        <div>
-          <TodoForm handleSubmit={addItem} />
-        </div>
         ​
-        <div>
-          <TodoList
-            list={list}
-            handleComplete={toggleComplete}
-            handleDelete={togglehandleDelete}
-          />
+        <div style={{ textAlign: 'end' }}>
+          <Button
+            style={{}}
+            variant="primary"
+            onClick={(e) => siteContext.setShow(!siteContext.show)}
+          >
+            Hide Completed
+          </Button>
+          <form onChange={(e) => siteContext.changeSort(e.target.value)}>
+            <label class="radio">
+              <input type="radio" name="sort" value="difficulty" />
+              difficulty
+            </label>
+            <label class="radio">
+              <input type="radio" name="sort" value="complete" />
+              complete
+            </label>
+            <label class="radio">
+              <input type="radio" name="sort" value="assignee" />
+              assignee
+            </label>
+            <label class="radio">
+              <input type="radio" name="sort" value="none" />
+              none
+            </label>
+          </form>
         </div>
-      </section>
+        <section className="todo">
+          <Auth capability="create">
+            <div>
+              <TodoForm handleSubmit={addItem} />
+            </div>
+          </Auth>
+          ​
+          <div>
+            <TodoList
+              list={list}
+              handleComplete={toggleComplete}
+              handleDelete={togglehandleDelete}
+            />
+          </div>
+        </section>
+      </Auth>
     </main>
   );
 }
